7e86ca7d5c36a41731d4ea7e42b34eef08ae65ee
kryptonxarmy <ismynamedanu@gmail.com>
2023-12-28 16:32:44 +0700

    tampilan input fix

--- resources/views/Kaprodi/inputfile.blade.php
+++ resources/views/Kaprodi/inputfile.blade.php
@@ -12,34 +12,40 @@
 <body>
     <div class="bg-[#E7ECEF] h-[200vh] flex-col flex gap-6" data-theme="light">
         @include('components.navbar')
-        <div class="mt-[7em] bg-red-400">
-            <form action="" class="" method="post">
-                <div class="w-full mx-auto">
-                    @csrf
-                    <h1 class="text-2xl font-bold mb-4">Input Data Mahasiswa</h1>
-                    <div class="mb-4 w-full">
-                        <label for="nama_mahasiswa" class="block text-sm font-medium text-gray-600">Nama
-                            Mahasiswa</label>
-                        <input type="text" id="nama_mahasiswa" name="nama_mahasiswa" placeholder="Type here"
-                            class="input input-bordered w-full max-w-xs" required />
-                    </div>
-                    <div class="mb-4 w-full">
-                        <label for="nim" class="block text-sm font-medium text-gray-600">NIM</label>
-                        <input type="text" id="nim" name="nim" placeholder="Type here"
-                            class="input input-bordered w-full max-w-xs" required />
-                    </div>
-                    <div class="mb-4 w-full">
-                        <label for="kelas" class="block text-sm font-medium text-gray-600">Kelas</label>
-                        <input type="text" id="kelas" name="kelas" placeholder="Type here"
-                            class="input input-bordered w-full max-w-xs" required />
-                    </div>
-                    <div class="mb-4 w-full">
-                        <label for="angkatan" class="block text-sm font-medium text-gray-600">Angkatan</label>
-                        <input type="text" id="angkatan" name="angkatan" placeholder="Type here"
-                            class="input input-bordered w-full max-w-xs" required />
-                    </div>
-                    <button type="submit" class="btn btn-primary w-full max-w-xs">Submit</button>
+        <div class="mt-[7em] px-[7em ]">
+            <a href="/dashboardkaprodi">
+                <span class="material-symbols-outlined ml-[8em] hover:cursor-pointer">
+                    arrow_back
+                </span>
+            </a>
+            <form action="" class="w-full flex flex-col items-center" method="post">
+                @csrf
+                <h1 class="text-2xl font-bold mb-4">Input Data Mahasiswa</h1>
+                <div class="mb-4 w-[60%]">
+                    <label for="nama_mahasiswa" class="block text-sm font-bold text-black">Nama
+                        Mahasiswa</label>
+                    <input type="text" id="nama_mahasiswa" name="nama_mahasiswa" placeholder="Type here"
+                        class="input input-bordered w-full" required />
                 </div>
+                <div class="mb-4 w-[60%]">
+                    <label for="nim" class="block text-sm font-bold text-black ">NIM</label>
+                    <input type="text" id="nim" name="nim" placeholder="Type here"
+                        class="input input-bordered w-full " required />
+                </div>
+                <div class="mb-4 w-[60%]">
+                    <label for="kelas" class="block text-sm font-bold text-black ">Kelas</label>
+                    <input type="text" id="kelas" name="kelas" placeholder="Type here"
+                        class="input input-bordered w-full " required />
+                </div>
+                <div class="mb-4 w-[60%]">
+                    <label for="angkatan" class="block text-sm font-bold text-black ">Angkatan</label>
+                    <input type="text" id="angkatan" name="angkatan" placeholder="Type here"
+                        class="input input-bordered w-full " required />
+                </div>
+                <div class="w-[40%] mt-4">
+                    <button type="submit" class="btn btn-primary w-full">  Submit</button>
+                </div>
+
             </form>
         </div>
     </div>
