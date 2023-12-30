589d67d6b9da5b3e5c3d2e67c571e8d8df8f3598
kryptonxarmy <ismynamedanu@gmail.com>
2023-12-30 20:09:22 +0700

    habis di merge

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-
 // class User extends Model
 class User extends Authenticatable
 {
